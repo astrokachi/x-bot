a0d9f11e89972544802a81d65fc05a9551330ab1
kachi <astrokachi@gmail.com>
2025-12-01 02:34:09 +0100

    chore: refactor

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,40 @@
 import "dotenv/config";
 import express from 'express';
 import routes from './routes/reply-tweet.route';
-import { logger } from './utils/logger';
+import authRoute from './routes/auth.route'
 import { requestLogger } from './middleware/request-logger';
+import session from "express-session";
 
 const PORT = process.env.PORT || 8080
 const app = express();
 app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+
+
 if (!process.env.GITHUB_TOKEN) {
-  console.error("GITHUB_TOKEN is not set. Set it in your environment or .env file.");
+  console.error("GITHUB_TOKEN is not set.");
 }
 
-if (!process.env.OAUTH_CLIENT_ID || !process.env.OAUTH_CLIENT_SECRET) {
-  console.error("OAUTH_CLIENT_ID and OAUTH_CLIENT_SECRET are not set. Set them in your environment or .env file.");
+if (!process.env.X_CLIENT_ID) {
+  console.error("X_CLIENT_ID not set.");
 }
 
 app.use(requestLogger);
 
 app.use("/api", routes)
+app.use("/auth", authRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
