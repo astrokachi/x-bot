2d0a89d4dac2d37b0657eca070ae1b4825e52c46
Ozoemena Ekenedirichukwu Onyedikachi <astrokachi@gmail.com>
2026-01-17 00:22:51 +0100

    Delete dist directory

--- dist/features/auth/auth.route.js
+++
@@ -1,7 +0,0 @@
-import { authorize, getToken, logout } from "./auth.controller.js";
-import { Router } from "express";
-const router = Router();
-router.get("/authorize", authorize);
-router.get("/callback", getToken);
-router.post("/logout", logout);
-export default router;
--- dist/features/tweet-reply/tweet-reply.route.js
+++
@@ -1,5 +0,0 @@
-import { replyToTweets } from './tweet-reply.controller.js';
-import { Router } from 'express';
-const router = Router();
-router.post('/reply', replyToTweets);
-export default router;
