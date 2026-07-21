6d6fb8c16c9d06fbc97b997feed3ed3909454ba6
kachi <astrokachi@gmail.com>
2026-07-22 00:14:20 +0100

    cleanup obsolete consts and types

--- src/shared/types/auth.ts
+++ src/shared/types/auth.ts
@@ -1,28 +1,6 @@
-export type OauthParamsInput = {
-  code?: string;
-  codeChallenge?: string;
-  codeVerifier?: string;
-  state?: string
-}
-
-export type PKCEPair = {
-  codeVerifier: string;
-  codeChallenge: string;
-}
-
 export type Tokens = {
   accessToken: string;
   refreshToken: string;
-}
-
-export type XTokens = {
-  access_token: string;
-  refresh_token: string;
-}
-
-export type RedisSessionInput = {
-  tokens: Tokens;
-  sessionID: string;
 }
 
 export type XUser = {
